caebfea37f7344acbe1ef96db365a202d88808d5
silence <silence.since2017@gmail.com>
2017-11-10 15:51:30 +0800

    update crawler

--- Download.py
+++ Download.py
@@ -1,15 +1,19 @@
 import re
 import requests
 import random
-import time 
+import time
+
 
 class download():
     def __init__(self):
         self.iplist = []
-        ip_response = requests.get('http://www.xicidaili.com',headers={'User-Agent':"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3"})
-        iplistn = re.findall(r'((?:(?:25[0-5]|2[0-4]\d|(?:1\d{2}|[1-9]?\d))\.){3}(?:25[0-5]|2[0-4]\d|(?:1\d{2}|[1-9]?\d)))',ip_response.text)
+        ip_response = requests.get('http://www.xicidaili.com/wt/', headers={
+                                   'User-Agent': "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3"})
+        iplistn = re.findall(
+            r'(\d+\.\d+\.\d+\.\d+\<\/td\>\s+\<td\>\d+)', ip_response.text)
         for ip in iplistn:
-            self.iplist.append(ip)
+            s = ip.replace('</td>\n      <td>', ':')
+            self.iplist.append(s)
         self.user_agent_list = [
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
             "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
@@ -31,48 +35,33 @@ class download():
             "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
         ]
 
-    def get(self,url,timeout=3,proxy=None,num_retries=6):
+    def get(self, url, proxy=None, num_retries=2, timeout=15):
         UA = random.choice(self.user_agent_list)
-        headers = {'User-Agent':UA}
+        headers = {'User-Agent': UA}
 
         if proxy == None:
             try:
-                return requests.get(url,headers=headers,timeout=timeout)
-            except:
-                if num_retries > 0:
-                    #time.sleep(1)
-                    print(u'获取网页出错，获取倒数第',num_retries,u'次')
-                    return self.get(url,timeout,num_retries-1)
-                else:
-                    print(u'开始使用代理')
-                    #time.sleep(1)
-                    IP = random.choice(self.iplist)
-                    proxy={'http':IP}
-                    return self.get(url,timeout,proxy,6)
+                return requests.get(url, headers=headers, timeout=timeout)
+            except Exception as e:
+                print(e)
+                print(u'开始使用代理')
+                # time.sleep(1)
+                IP = random.choice(self.iplist)
+                proxy = {'http': IP}
+                return self.get(url, proxy)
         else:
             try:
-                IP = random.choice(self.iplist)
-                proxy={'http':IP}
-                return requests.get(url,headers=headers,proxies=proxy,timeout=timeout)
-            
+                print(u'当前代理是', proxy)
+                return requests.get(url, headers=headers, proxies=proxy, timeout=timeout)
             except:
-                if num_retries>0:
-                    #time.sleep(1)
+                if num_retries > 0:
+                    # time.sleep(1)
                     IP = random.choice(self.iplist)
-                    proxy={'http':IP}
-                    print(u'正在更换代理,获取第倒数第',num_retries,u'次')
-                    print(u'当前代理是:',proxy)
-                    return self.get(url,timeout,proxy,num_retries-1)
+                    proxy = {'http': IP}
+                    print(u'正在更换代理,获取第倒数第', num_retries, u'次')
+                    return self.get(url, proxy, num_retries - 1)
                 else:
-                    print(u'ip被ban完了，GG')
+                    print(u'估计是其他原因不能下载')
+
 
 download = download()
-
-
-
-
-
-
-
-        
-
